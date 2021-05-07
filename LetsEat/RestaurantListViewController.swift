215611c775de2f60d3b78bbd7a19e9214c1f538c
joycelin12 <joycelin12@hotmail.com>
2021-05-06 21:17:27 -0400

    try to link restaurant list controller instead of restaurant view controller but having error Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: '-[UIView collectionView:numberOfItemsInSection:]: unrecognized selector sent to instance 0x7fb1ab42ae90' will figure out next time

--- LetsEat/RestaurantListViewController.swift
+++ LetsEat/RestaurantListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RestaurantListViewController: UIViewController {
+class RestaurantListViewController: UIViewController, UICollectionViewDataSource {
     
     @IBOutlet var collectionView:UICollectionView!
 
@@ -16,6 +16,18 @@ class RestaurantListViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
 
    
